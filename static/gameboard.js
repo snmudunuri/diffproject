bed4e4be214bc19cbc3ab45331b1482a516558e7
Shirdi Nagaraju Mudunuri <Mudunuri@civicplus.com>
2019-07-26 11:14:24 -0500

    Changes to the user input and placed restart and refresh button

--- static/gameboard.js
+++ static/gameboard.js
@@ -5,86 +5,84 @@ let player2Selection = [];
 let points1 = 0;
 let points2 = 0;
 let winnerCombination = new Array();
+let input = prompt("Choose the matrix you want to play", "3 OR 4 OR 5");
 
-function loadAnswers(){
-  let n = prompt("Choose the matrix you want to play", "3 OR 4 OR 5");
-
-  while (n<3 || n>5){
-    n = prompt("Please choose valid matrix", "3 OR 4 OR 5");
-  }
-
-winnerCombination = [];
-let answersSet = new Array();
-let current = 0;
-
-for (let i = 1; i <= n * n; i++) {
-  answersSet.push(i);
-  if (answersSet.length == n) {
-    winnerCombination.push(answersSet);
-    answersSet = [];
-  }
+while (input < 3 || input > 5) {
+  input = prompt("Please choose valid matrix", "3 OR 4 OR 5");
 }
 
-for (let i = 1; i <= n; i++) {
-  answersSet.push(i);
-  current = i;
+let n = parseInt(input);
+function loadAnswers() {
+  winnerCombination = [];
+  let answersSet = new Array();
+  let current = 0;
 
-  for (let j = 1; j < n; j++) {
-    answersSet.push(current + n);
-    current = current + n;
-  }
-
-  if (answersSet.length == n) {
-    winnerCombination.push(answersSet);
-    answersSet = [];
-  }
-}
-
-for (let i = 1; i <= n * n; i++) {
-  for (let j = 1; j <= n * n; j++) {
-    if (i - j == 0) {
-      answersSet.push(i);
-      current = i;
-      for (let r = 1; r < n; r++) {
-        answersSet.push(current + n + 1);
-        current = current + n + 1;
-      }
-      if (answersSet.length == n) {
-        winnerCombination.push(answersSet);
-      }
+  for (let i = 1; i <= n * n; i++) {
+    answersSet.push(i);
+    if (answersSet.length == n) {
+      winnerCombination.push(answersSet);
+      answersSet = [];
     }
   }
-  if (answersSet.length == n) {
-    answersSet = [];
-    break;
-  }
-}
 
-for (let i = 1; i <= n * n; i++) {
-  for (let j = 1; j <= n * n; j++) {
-    if (i + j == n + 1) {
-      answersSet.push(j);
-      current = j;
-      for (let r = 1; r < n; r++) {
-        answersSet.push(current + n - 1);
-        current = current + n - 1;
-      }
-      if (answersSet.length == n) {
-        winnerCombination.push(answersSet);
-      }
+  for (let i = 1; i <= n; i++) {
+    answersSet.push(i);
+    current = i;
+
+    for (let j = 1; j < n; j++) {
+      answersSet.push(current + n);
+      current = current + n;
+    }
+
+    if (answersSet.length == n) {
+      winnerCombination.push(answersSet);
+      answersSet = [];
     }
   }
-  if (answersSet.length == n) {
-    answersSet = [];
-    break;
-  }
-}
 
+  for (let i = 1; i <= n * n; i++) {
+    for (let j = 1; j <= n * n; j++) {
+      if (i - j == 0) {
+        answersSet.push(i);
+        current = i;
+        for (let r = 1; r < n; r++) {
+          answersSet.push(current + n + 1);
+          current = current + n + 1;
+        }
+        if (answersSet.length == n) {
+          winnerCombination.push(answersSet);
+        }
+      }
+    }
+    if (answersSet.length == n) {
+      answersSet = [];
+      break;
+    }
+  }
+
+  for (let i = 1; i <= n * n; i++) {
+    for (let j = 1; j <= n * n; j++) {
+      if (i + j == n + 1) {
+        answersSet.push(j);
+        current = j;
+        for (let r = 1; r < n; r++) {
+          answersSet.push(current + n - 1);
+          current = current + n - 1;
+        }
+        if (answersSet.length == n) {
+          winnerCombination.push(answersSet);
+        }
+      }
+    }
+    if (answersSet.length == n) {
+      answersSet = [];
+      break;
+    }
+  }
 }
 
 function gameBoard() {
   loadAnswers();
-  console.log(winnerCombination);
   let colCount = 1;
   const parentNode = document.getElementById("game");
   while (parentNode.hasChildNodes()) {
@@ -128,16 +126,11 @@ function gameBoard() {
       document.getElementById("player1").innerHTML = points1;
       document.getElementById("player2").innerHTML = points2;
       reset();
-    }
-
-    else if(player1Selection.length == Math.floor((n*n)/2)+1){
+    } else if (player1Selection.length == Math.floor((n * n) / 2) + 1) {
       reset();
-    }
-    else if(player2Selection.length == (n*n)/2){
+    } else if (player2Selection.length == (n * n) / 2) {
       reset();
-    }
-    
-    else {
+    } else {
       if (currentPlayer == 0) {
         currentPlayer = 1;
       } else {
@@ -147,14 +140,11 @@ function gameBoard() {
   };
   for (let i = 1; i < colCount; i++) {
     const colId = document.getElementById(i);
-    //console.log(colId);
     colId.addEventListener("click", assign);
   }
-  // console.log(player1Selection.sort());
 }
 
 function winnerCheck() {
-  //  return false;
   let playerSelection = new Array();
   let playerWon = false;
 
@@ -200,4 +190,22 @@ function reset() {
   gameBoard();
 }
 
+function restart() {
+  currentPlayer = 0;
+  player1Selection = new Array();
+  player2Selection = new Array();
+  points1 = 0;
+  points2 = 0;
+  document.getElementById("player1").innerHTML = points1;
+  document.getElementById("player2").innerHTML = points2;
+  gameBoard();
+}
+
 window.addEventListener("load", gameBoard);
+
+const restartButton = document.getElementById("restart");
+const refreshButton = document.getElementById("refresh");
+restartButton.addEventListener("click", restart);
+refreshButton.addEventListener("click", e => {
+  location.reload(true);
+});
